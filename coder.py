6af633c40199ab25c43f0ccecab752798409ff05
James Margolis <margolisjames141@gmail.com>
2026-04-08 18:44:31 +0300

    Исправление генерации и распознования кьюар-кодов

--- coder.py
+++ coder.py
@@ -6,6 +6,6 @@ with open('registry.txt', 'r', encoding='UTF-8') as file:
     for line in file:
         info = line.strip("\n")
         img = segno.make(info, micro=False)
-        img.save('../codes/' + str(n) + '.png')
+        img.save('qr-codes/' + str(n) + '.png')
         n += 1
 
